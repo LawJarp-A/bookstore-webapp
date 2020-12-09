80e7bd31d4790a901fe16401c52dc3d3cfc4783a
LawJarp-A <prajwalanagani@gmail.com>
2020-12-09 11:44:23 +0530

    Added login pgae

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,23 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+
+import TextField from "@material-ui/core/TextField";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+
+import "./loginpage.css";
 
 const axios = require("axios");
 
 function LoginPage() {
+  var fields = {};
+  const [error, setError] = useState();
   function login() {
     axios
       .post("http://localhost:5001/api/login", {
-        email: "prajwalanagani@gmail.com",
-        password: "pass",
+        email: fields.email,
+        password: fields.password,
       })
       .then((res) => {
         console.log(res.data);
+        setError("Success");
         localStorage.setItem('accessToken', res.data.accessToken);
+      },
+      (err)=>{
+        setError(err.message)
       });
   }
   return (
-    <div className="LoginPage">
-      <h1>Work in progress</h1>
-      <button onClick={login}>LOGIN</button>
+    <div className="loginpage">
+      <Card>
+        <CardContent>
+          <div className="fields">
+            <div className="fields">
+              <label>Email</label>
+            </div>
+            <TextField
+              required
+              id="email"
+              label="Required"
+              variant="outlined"
+              onChange={(event) => {
+                fields[event.target.id] = event.target.value;
+              }}
+            />
+          </div>
+          <div className="fields">
+            <div className="fields">
+              <label>Password</label>
+            </div>
+            <TextField
+              required
+              id="password"
+              label="Required"
+              variant="outlined"
+              onChange={(event) => {
+                fields[event.target.id] = event.target.value;
+              }}
+            />
+          </div>
+          <div className="fields">{error}</div>
+        </CardContent>
+        <CardActions>
+          <Button variant="outlined" onClick={login}>
+            Login
+          </Button>
+          <Button variant="outlined" href="/register">Register</Button>
+        </CardActions>
+      </Card>
     </div>
   );
 }
